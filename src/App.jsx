373e252dbcda1fe24a52ebfbc6d0c3d7a7fb9d07
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-20 00:23:40 +0200

    add store

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,23 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Header } from './components';
 import { ROUTES } from './helpers';
 import { lazy } from 'react';
+import { useEffect } from 'react';
+import { getUserInfo } from './service';
 
 const Home = lazy(() => import('pages/Home'));
 const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async position => {
+      try {
+        const geo = await getUserInfo(position.coords);
+        console.log('geo: ', geo.results[0].annotations.currency.iso_code);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }, []);
   return (
     <>
       <Routes>
--- src/service/opencagedataApi.js
+++ src/service/opencagedataApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getUserInfo = async ({ latitude, longitude }) => {
-  const apiKey = 'd4683b09d0c94ec0aebf0b2e043decbf';
+  const apiKey = '096c22af6e544eb39377a5bfbe7173cc';
   const urlPosition = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}`;
   const { data } = await axios.get(urlPosition, {
     params: {

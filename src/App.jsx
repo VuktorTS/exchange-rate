b385b72a66f28534808c7ae8071fd279c104e656
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-19 13:57:58 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import { Heading } from 'components';
+// import { Heading } from 'components';
+import Home from 'pages/Home';
+import Rates from 'pages/Rates';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components';
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  // return <Heading title="Just do it!" />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/rates" element={<Rates />}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
 };

059a3fb226374799a6d5c7f0655c714413cea489
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-28 20:45:01 +0200

    Update exchangeAPI.js

--- src/service/exchangeAPI.js
+++ src/service/exchangeAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://api.apilayer.com/exchangerates_data/',
-  headers: { apikey: '' },
+  headers: { apikey: 'el8nnJtDUHz0eweC5U556WpvOZZJRI9H' },
 });
 
 export const exchangeCurrency = async credentials => {

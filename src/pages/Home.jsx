1f7e39c786818f3c2644e0efa9dfdb7209f6dcb6
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-28 20:44:35 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
-import { Container, ExchangeForm, Heading, Section } from 'components';
+import {
+  Container,
+  ExchangeForm,
+  ExchangeInfo,
+  Heading,
+  Section,
+} from 'components';
+import { useSelector } from 'react-redux';
+import { selectExchangeInfo } from 'reduxState/currency/selectors';
 
 const Home = () => {
   const isError = false;
+  const exchangeInfo = useSelector(selectExchangeInfo);
 
   return (
     <Section>
       <Container>
         <Heading info title="What currencies do you want to exchange?🙂" />
         <ExchangeForm />
-
+        {exchangeInfo && (
+          <ExchangeInfo
+            amount={exchangeInfo.amount}
+            from={exchangeInfo.from}
+            to={exchangeInfo.to}
+            rate={exchangeInfo.rate}
+            result={exchangeInfo.result}
+          />
+        )}
         {isError && (
           <Heading
             error

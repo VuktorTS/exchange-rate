1b05965b3d0b4d9fad1b3824faca88a13b2c74b5
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-28 18:49:59 +0200

    add handleSubmit and input params

--- src/components/ExchangeForm/ExchangeForm.jsx
+++ src/components/ExchangeForm/ExchangeForm.jsx
@@ -2,13 +2,26 @@ import { RiExchangeDollarFill } from 'react-icons/ri';
 import styles from './ExchangeForm.module.css';
 
 export const ExchangeForm = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    const { value } = event.target.elements.currency;
+    console.log('value: ', value);
+  };
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <button className={styles.button} type="submit">
         <RiExchangeDollarFill className={styles.icon} />
       </button>
 
-      <input title="Request format 15 USD in UAH" className={styles.input} />
+      <input
+        pattern="^\d+(\.\d{1,2})?\s[a-zA-Z]{3}\sin\s[a-zA-Z]{3}$"
+        title="Request format 15 USD in UAH"
+        className={styles.input}
+        placeholder="15 USD in UAH"
+        name="currency"
+        required
+      />
     </form>
   );
 };

5b5a811b0e290ffd90f9b7d72849eab922b6676e
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-03-01 00:37:56 +0200

    added thunk fetchLatestRates

--- src/reduxState/currency/operation.js
+++ src/reduxState/currency/operation.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { exchangeCurrency } from 'service/exchangeAPI';
+import { exchangeCurrency, latestRates } from 'service/exchangeAPI';
 import { getUserInfo } from 'service/opencagedataApi';
 
 export const fetchBaseCurrency = createAsyncThunk(
@@ -19,10 +19,21 @@ export const fetchBaseCurrency = createAsyncThunk(
   },
 );
 export const fetchExchangeCurrency = createAsyncThunk(
-  'currincy/fetchExchangeCurrency',
+  'currency/fetchExchangeCurrency',
   async (credentials, thunkApi) => {
     try {
       const data = await exchangeCurrency(credentials);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  },
+);
+export const fetchLatestRates = createAsyncThunk(
+  'currency/fetchExchangeRatesCurrency',
+  async (baseCurrency, thunkApi) => {
+    try {
+      const data = await latestRates(baseCurrency);
       return data;
     } catch (error) {
       return thunkApi.rejectWithValue(error.message);

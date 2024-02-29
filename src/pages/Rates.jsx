30bbb9ab8cfb11dbaadd60ac531fe5c6151e9a28
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-03-01 00:36:43 +0200

    added selector and dispatch

--- src/pages/Rates.jsx
+++ src/pages/Rates.jsx
@@ -1,9 +1,27 @@
 import { Wave } from 'react-animated-text';
 
-import { Container, Heading, Section } from 'components';
+import { Container, Heading, Loader, RatesList, Section } from 'components';
+import {
+  selectBaseCurrency,
+  selectIsError,
+  selectIsLoading,
+  selectVisibleRates,
+} from 'reduxState/currency/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchLatestRates } from 'reduxState/currency/operation';
 
 const Rates = () => {
-  const isError = false;
+  const isLoading = useSelector(selectIsLoading);
+  const isError = useSelector(selectIsError);
+  const fileredRates = useSelector(selectVisibleRates);
+  const baseCurrency = useSelector(selectBaseCurrency);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchLatestRates(baseCurrency));
+  }, [baseCurrency, dispatch]);
 
   return (
     <Section>
@@ -20,12 +38,14 @@ const Rates = () => {
           }
         />
 
+        {fileredRates.length > 0 && <RatesList rates={fileredRates} />}
         {isError && (
           <Heading
             error
             title="Something went wrong...😐 We cannot show current rates!"
           />
         )}
+        {isLoading && <Loader />}
       </Container>
     </Section>
   );
